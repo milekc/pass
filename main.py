2fac41c1de080d38318188acbe0e1f96ac2d0092
milekc <milekc@yandex.ru>
2022-01-30 18:11:42 +0300

    создан шаблон

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from docxtpl import DocxTemplate
 
+
+class Passport:
+    def __init__(self):
+        self.doc = DocxTemplate('temp.docx')
+        self.context = {'name': 'Кантователь плит'}
+
+    def run(self):
+        self.doc.render(self.context)
+        self.doc.save('pass.docx')
+
+
+if __name__ == '__main__':
+
+    pas = Passport()
+    pas.run()
